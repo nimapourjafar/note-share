3d3d487a0e1933eea885098443f158e61ae2c25d
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-07-30 01:25:07 -0400

    use callback?

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import {useCallback} from 'react'
+import Quill from 'quill'
+import "quill/dist/quill.snow.css"
 
 const TextEditor = () => {
+    const wrapperRef = useCallback((wrapper) => {
+        if (wrapper == null) return
+        wrapper.innerHTML = ''
+        const editor = document.createElement('div')
+        wrapper.append(editor)
+        const quill = new Quill(editor, {theme:"snow"})
+
+    },[])
     return (
-        <div>
-            Text Editor
+        <div id="editor" ref={wrapperRef}>
+            
         </div>
     )
 }
