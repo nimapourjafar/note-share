1ef096b01c809cfc734164803438396db940fd82
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-07-31 21:04:51 -0400

    server setup done

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,34 +1,68 @@
-import {useCallback} from 'react'
-import Quill from 'quill'
-import "quill/dist/quill.snow.css"
+import { useCallback, useEffect, useState } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+import { io } from "socket.io-client";
 
 const TOOLBAR_OPTIONS = [
-    [{ header: [1, 2, 3, 4, 5, 6, false] }],
-    [{ font: [] }],
-    [{ list: "ordered" }, { list: "bullet" }],
-    ["bold", "italic", "underline"],
-    [{ color: [] }, { background: [] }],
-    [{ script: "sub" }, { script: "super" }],
-    [{ align: [] }],
-    ["image", "blockquote", "code-block"],
-    ["clean"],
-  ]
-  
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+];
 
 const TextEditor = () => {
-    const wrapperRef = useCallback((wrapper) => {
-        if (wrapper == null) return
-        wrapper.innerHTML = ''
-        const editor = document.createElement('div')
-        wrapper.append(editor)
-        const quill = new Quill(editor, {theme:"snow", modules : {toolbar: TOOLBAR_OPTIONS }})
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
 
-    },[])
-    return (
-        <div className="container" ref={wrapperRef}>
-            
-        </div>
-    )
-}
+  useEffect(() => {
+    const s = io("http://localhost:3001");
+    setSocket(s);
+    return () => {
+      s.disconnect();
+    };
+  }, []);
 
-export default TextEditor
+  useEffect(() => {
+    if (socket==null || quill==null) return
+    const handler = (delta, oldDelta, source) => {
+      if (source !== "user") return;
+      socket.emit("send-changes", delta);
+    };
+    quill.on("text-change", handler);
+    return () => {
+      quill.off("text-change", handler);
+    };
+  }, [quill,socket]);
+
+  useEffect(() => {
+    if (socket==null || quill==null) return
+    const handler = (delta) => {
+      quill.updateContents(delta);
+    };
+    socket.on("recieve-changes", handler);
+    return () => {
+      quill.off("recieve-changes", handler);
+    };
+  }, [quill,socket]);
+
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    const quill = new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    });
+    setQuill(quill);
+  }, []);
+
+  return <div className="container" ref={wrapperRef}></div>;
+};
+
+export default TextEditor;
---
+++ server/server.js
@@ -0,0 +1,13 @@
+const io = require('socket.io')(3001, {
+    cors:{
+        origin:'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+})
+
+io.on('connection',socket=> {
+    console.log('a user connected')
+    socket.on('send-changes',(delta)=> {
+        socket.broadcast.emit("recieve-changes",delta)
+    })
+})
